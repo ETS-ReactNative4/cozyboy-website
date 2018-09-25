9d2b81b9a5eb691a5db65caacbf3befce06266db
Conner Frey <connerfrey@conners-mbp-2.lan>
2018-09-25 01:44:51 -0700

    fixed a bedroom story error

--- src/components/CozyBedroomStory.js
+++ src/components/CozyBedroomStory.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import CozyBedroomStory from './CozyBedroomStory';
 
 export default class CozyBedroomStory extends Component {
   LinkWrapper = ({text, href}) => (
